038ab4f0f789f9a69378ce465583915dc128b12a
Sebastien Darche <sebastien.darche@free.fr>
2025-09-23 15:15:48 -0400

    Additional demos

--- 05-openmp/include/utils.h
+++ 05-openmp/include/utils.h
@@ -16,6 +16,16 @@ void random_fill(std::vector<float>& vec, float max=100.f) {
     }
 }
 
+void random_fill(float* vec, size_t n, float max=100.f) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dist(0.f, max);
+    for(auto i = 0; i < n; ++i) {
+        vec[i] = dist(gen);
+    }
+}
+
+
 template <typename T>
 long count_ms(T duration) {
     return std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
